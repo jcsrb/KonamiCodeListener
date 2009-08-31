729a3e201918f9a268d9e7a6e60e7cf927431626
Midday <Jakob@Jakob-PC>
2009-09-01 02:05:31 +0300

    working version 0.1

--- src/com/github/midday/kcl/KonamiCodeListener.java
+++ src/com/github/midday/kcl/KonamiCodeListener.java
@@ -1,5 +1,36 @@
 package com.github.midday.kcl;
 
-public class KonamiCodeListener {
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
+public class KonamiCodeListener extends KeyAdapter {
+
+	final static int[] BUTTONLIST = { KeyEvent.VK_UP, KeyEvent.VK_UP,
+			KeyEvent.VK_DOWN, KeyEvent.VK_DOWN, KeyEvent.VK_LEFT,
+			KeyEvent.VK_RIGHT, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT,
+			KeyEvent.VK_B, KeyEvent.VK_A };
+	int count = 0;
+
+	public void keyPressed(KeyEvent evt) {
+		// Check for the KonamiSequece
+		if (evt.getKeyCode() == BUTTONLIST[count]) {
+			count++;
+		} else {
+			count = 0;
+		}
+		// if the code checks out startSecret(evt.getSource())
+		if (count > 9) {
+			startSecret(evt.getSource());
+			//reset counter
+			count =0;
+		}
+
+
+
+	}
+
+	public void startSecret(Object source) {
+		// Overwrite this method
+		System.out.println("KonamiCode entered correctly");
+	}
 }
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,13 +1,47 @@
 package test;
 
-public class Test {
+//importing all our swing needs
+import javax.swing.*;
 
-	/**
-	 * @param args
-	 */
+//importing the KonamiCodeListener
+import com.github.midday.kcl.KonamiCodeListener;
+
+public class Test {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Schedule a job for the event-dispatching thread:
+		// creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
 
 	}
 
+	static void createAndShowGUI() {
+		// creating our test frame
+		JFrame testframe = new JFrame();
+		// setting a size
+		testframe.setSize(400, 300);
+		// moving it to the center of the screen
+		testframe.setLocationRelativeTo(null);
+		// setting a nice title
+		testframe.setTitle("KonamiCodeListener Test");
+		// make it close the process if we press the exit button
+		testframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// make it focusable so the KeyListener reacts
+		testframe.setFocusable(true);
+		// add the KeyListener
+		testframe.addKeyListener(new MyKCL());
+		// make the frame visible
+		testframe.setVisible(true);		
+	}
+
+}
+
+class MyKCL extends KonamiCodeListener {
+	// overwrites the original startSecret method
+	public void startSecret(Object source) {
+		JOptionPane.showMessageDialog(null, "Konami!", "Activated",1);		 
+	}
 }
